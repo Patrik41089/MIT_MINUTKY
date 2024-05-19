ea4211c01587cbc2433043750a18d0c769272302
Patrik Radvák <rad41089@spseol.cz>
2024-05-19 14:52:17 +0200

    poslani kodu

--- src/main.c
+++ src/main.c
@@ -1,45 +1,96 @@
-#include <stdbool.h>
-#include <stm8s.h>
-//#include <stdio.h>
-#include "main.h"
-#include "milis.h"
-//#include "delay.h"
-//#include "uart1.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "stm8s.h"
 
-void init(void)
-{
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
+// Definice pinu pro akustický signál
+#define BUZZER_PIN GPIO_PIN_3
+#define BUZZER_PORT GPIOC
 
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+// Definice pinů pro enkoder
+#define ENCODER_PIN_A GPIO_PIN_1
+#define ENCODER_PIN_B GPIO_PIN_2
+#define ENCODER_PORT GPIOA
 
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
+// Maximální čas v minutách
+#define MAX_TIME 180
 
+// Proměnná pro uložení zbývajícího času
+uint8_t remaining_time = 0;
 
-    init_milis();
-    //init_uart1();
+// Inicializace periferií
+void init_peripherals() {
+    // Inicializace GPIO pro akustický signál
+    GPIO_Init(BUZZER_PORT, BUZZER_PIN, GPIO_MODE_OUT_PP_LOW_FAST);
+
+    // Inicializace GPIO pro enkoder jako vstupy s pull-up odpory
+    GPIO_Init(ENCODER_PORT, ENCODER_PIN_A | ENCODER_PIN_B, GPIO_MODE_IN_PU_IT);
+
+    // Nastavení interruptu pro enkoder
+    EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOA, EXTI_SENSITIVITY_FALL_ONLY);
+    EXTI_SetTLISensitivity(EXTI_TLISENSITIVITY_FALL_ONLY);
 }
 
+// Funkce pro inicializaci časovače
+void init_timer() {
+    // Nastavení časovače pro přerušení každou minutu
+    TIM4_TimeBaseInit(TIM4_PRESCALER_128, 125);
+    TIM4_ClearFlag(TIM4_FLAG_UPDATE);
+    TIM4_ITConfig(TIM4_IT_UPDATE, ENABLE);
+    TIM4_Cmd(ENABLE);
+}
 
-int main(void)
-{
-  
-    uint32_t time = 0;
+// Funkce pro zpracování akustického signálu
+void beep() {
+    GPIO_WriteReverse(BUZZER_PORT, BUZZER_PIN);
+    // Počkejte krátkou dobu, abyste mohli slyšet zvuk
+    for(int i = 0; i < 10000; i++);
+    GPIO_WriteReverse(BUZZER_PORT, BUZZER_PIN);
+}
 
-    init();
-
-    while (1) {
-#if defined (BTN_PORT) && defined (BTN_PIN)
-        if (milis() - time > 333 && !PUSH(BTN)) {
-#else
-        if (milis() - time > 333 ) {
-#endif
-            REVERSE(LED); 
-            time = milis();
-            //printf("%ld\n", time);
-        }
-        //delay_ms(333);
+// Funkce pro zpracování změny času
+void process_time_change(uint8_t direction) {
+    if (direction == 1 && remaining_time < MAX_TIME) {
+        remaining_time++;
+    } else if (direction == -1 && remaining_time > 0) {
+        remaining_time--;
     }
 }
 
-/*-------------------------------  Assert -----------------------------------*/
-#include "__assert__.h"
+// Obsluha přerušení od časovače (každou minutu)
+INTERRUPT_HANDLER(TIM4_UPD_OVF_IRQHandler, TIM4_UPD_OVF_IRQn) {
+    // Snížit zbývající čas
+    if (remaining_time > 0) {
+        remaining_time--;
+    }
+    // Pokud došlo k vypršení času, zavolejte funkci beep
+    if (remaining_time == 0) {
+        beep();
+    }
+    TIM4_ClearITPendingBit(TIM4_IT_UPDATE);
+}
+
+// Obsluha přerušení od enkoderu
+INTERRUPT_HANDLER(EXTI1_IRQHandler, EXTI1_IRQn) {
+    // Zjistěte směr otáčení enkoderu
+    uint8_t direction = 0;
+    if (GPIO_ReadInputPin(ENCODER_PORT, ENCODER_PIN_B) == RESET) {
+        direction = 1; // Otáčení doleva
+    } else {
+        direction = -1; // Otáčení doprava
+    }
+    process_time_change(direction);
+    // Vyčištění vlajky přerušení
+    EXTI_ClearITPendingBit(EXTI_IT_Pin1);
+}
+
+// Hlavní funkce
+int main() {
+    // Inicializace periferií
+    init_peripherals();
+    init_timer();
+    // Nekonečná smyčka
+    while (1) {}
+    return 0;
+}
