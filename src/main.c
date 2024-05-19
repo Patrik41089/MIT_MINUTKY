5319e12c11b3b83f8fec1ff98ea372b35931a43a
Patrik Radvák <rad41089@spseol.cz>
2024-05-19 14:15:21 +0200

    nic

--- src/main.c
+++ src/main.c
@@ -11,9 +11,9 @@ void init(void)
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
 
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-#if defined (BTN_PORT) || defined (BTN_PIN)
+
     GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-#endif
+
 
     init_milis();
     //init_uart1();
